6c6116235fbdf6ad44de88ff529eaa766ecfa33f
Karol <karolbolejniczak@gmail.com>
2020-10-05 20:07:25 +0200

    edited method name, added implementation dao

--- src/main/java/pl/karol/dao/Dao.java
+++ src/main/java/pl/karol/dao/Dao.java
@@ -2,7 +2,7 @@ package pl.karol.dao;
 
 public interface Dao<T> {
 
-	public void save(T t);
+	public void create(T t);
 	public T read(Integer id);
 	public void update(T t);
 	public void delete(Integer id);
--- src/main/java/pl/karol/dao/FriendDaoImpl.java
+++ src/main/java/pl/karol/dao/FriendDaoImpl.java
@@ -1,5 +1,9 @@
 package pl.karol.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
 import org.springframework.stereotype.Repository;
 
 import pl.karol.model.Friend;
@@ -7,28 +11,55 @@ import pl.karol.model.Friend;
 @Repository
 public class FriendDaoImpl implements Dao<Friend> {
 
+	private EntityManagerFactory entityManagerFactory;
+
 	@Override
-	public void save(Friend t) {
-		// TODO Auto-generated method stub
-		
+	public void create(Friend t) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+
+		entityTransaction.begin();
+		entityManager.persist(t);
+
+		entityTransaction.commit();
+		entityManager.close();
+
 	}
 
 	@Override
 	public Friend read(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+		Friend friend = entityManager.find(Friend.class, id);
+		entityManager.close();
+
+		return friend;
 	}
 
 	@Override
 	public void update(Friend t) {
-		// TODO Auto-generated method stub
-		
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+
+		entityTransaction.begin();
+		entityManager.merge(t);
+
+		entityTransaction.commit();
+		entityManager.close();
+
 	}
 
 	@Override
 	public void delete(Integer id) {
-		// TODO Auto-generated method stub
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
 		
+		Friend friend = entityManager.find(Friend.class, id);
+		
+		entityManager.remove(friend);
+		
+		entityTransaction.commit();
+		entityManager.close();
 	}
 
 }
