39d3d5bddd5f57436b7c669c153ee8a67cceddc5
Karol <karolbolejniczak@gmail.com>
2020-10-05 17:03:53 +0200

    created project, added properties

---
+++ src/test/java/pl/karol/FriendsBookApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.karol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
