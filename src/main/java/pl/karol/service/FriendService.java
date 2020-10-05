ecda25d562a36d69840ab59a003ee3f31cb3134c
Karol <karolbolejniczak@gmail.com>
2020-10-05 19:04:44 +0200

    deleted repository layer and corrected Service

--- src/main/java/pl/karol/service/FriendService.java
+++ src/main/java/pl/karol/service/FriendService.java
@@ -3,11 +3,12 @@ package pl.karol.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import pl.karol.repository.FriendRepository;
+import pl.karol.dao.Dao;
+import pl.karol.model.Friend;
 
 @Service
 public class FriendService {
 	
 	@Autowired
-	private FriendRepository friendRepository;
+	private Dao<Friend> friendDao;
 }
